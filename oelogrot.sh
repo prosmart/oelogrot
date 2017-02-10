9224fae156aadfc1d461e16040a274599d302616
prosmart <dna@edrs.com.au>
2017-02-10 12:10:39 +1000

    Added additional logic to allow for purging of logs over n weeks. Also added logic to allow filtering of logs using "-filter" and key words in etc/logrotate.cfg

--- oelogrot.sh
+++ oelogrot.sh
@@ -11,6 +11,10 @@
 # Un-comment following line for debugging
 # ECHO=echo
 
+# Change to true to get additional debugging.
+debug=false
+
+
 if [ -z "$PROTOP" ]; then
     echo "PROTOP environment variable not set - exiting"
     exit 1
@@ -24,6 +28,8 @@ TMPDIR=${TMPDIR-/tmp}
 LOGDIR=${LOGDIR-/tmp}
 LGARCDIR=${LGARCDIR-/tmp}
 NOW='date "+%Y.%m.%d %H:%M:%S"'
+RENUM='^[0-9]+$'
+DBLIST=${PROTOP}/etc/dblist.cfg
 					# Check directories exist or create them
 [ ! -d ${TMPDIR} ] && mkdir ${TMPDIR}
 [ ! -d ${LOGDIR} ] && mkdir ${LOGDIR}
@@ -75,6 +81,14 @@ roll_logs()				# roll the logs function
    then
       ${PROTOP}/bin/sendalert.sh $FRNAME -msg="prolog failed during log roll" 
    fi
+
+   if $FILT = true	# We are filtering
+   then
+      if [ -f $LOGROTCFG.tmp ]
+      then
+         $ECHO grep -v -f $LOGROTCFG.tmp ${LGARCDIR}/${FRNAME}.lg.${WK} >${LGARCDIR}/${FRNAME}.lgf.${WK}
+      fi
+   fi
 }
 
 #
@@ -94,6 +108,47 @@ then
    usage
 fi
 
+# Are we filtering?
+
+if [ ! -z "${2}" ]  && [ "${2}" = "-filter" ]
+then
+   FILT=true
+else
+   FILT=false
+fi
+
+if [ $debug == "true" ]; then
+   echo '${2} = ' ${2}
+   echo Filter = $FILT
+   read cont
+fi
+
+LOGROTCFG=$PROTOP/etc/logrotate.cfg
+if $FILT = true 
+then
+   if [ -f $LOGROTCFG ]
+   then
+      LOGHIST=`grep -i "^loghist" $LOGROTCFG|sed 's/^.*=//'`
+      FLOGHIST=`grep -i "^floghist" $LOGROTCFG|sed 's/^.*=//'`
+      grep "^(.*)$" $LOGROTCFG >$LOGROTCFG.tmp
+      if [[ ! $LOGHIST =~ $RENUM ]] || [ -z $LOGHIST ]
+      then
+         LOGHIST=0
+      fi
+      if [[ ! $FLOGHIST =~ $RENUM ]]
+      then
+         FLOGHIST=0
+      fi
+   fi
+fi
+
+if [ $debug == "true" ]; then
+   echo After logrotate.cfg
+   echo LOGHIST = $LOGHIST
+   echo FLOGHIST = $FLOGHIST
+   read x
+fi
+
 DB=
 
 # First look for the literal "all" to signify all databases in etc/dblist.cfg
@@ -104,12 +159,12 @@ DB=
 if [ "${IP1}" = "all" ]
 then
 
-   if ! [ -f ${PROTOP}/etc/dblist.cfg ]
+   if ! [ -f $DBLIST ]
    then
       errmsg "'all' option used and no dblist.cfg present"
       exit 1
    fi
-   grep -v -e "^#" -e "^ " -e "^$" ${PROTOP}/etc/dblist.cfg | while IFS="|" read -r FRNAME DB col3andtherest
+   grep -v -e "^#" -e "^ " -e "^$" $DBLIST | while IFS="|" read -r FRNAME DB col3andtherest
    do
       roll_logs ${DB}
    done
@@ -118,9 +173,9 @@ else
 
    # Is it a "friendly name" defined in etc/dblist.cfg?
 
-   if [ -f ${PROTOP}/etc/dblist.cfg ]   # search using "pipe" as the delimiter
+   if [ -f $DBLIST ]   # search using "pipe" as the delimiter
    then
-      DB=`grep "^${IP1}|" ${PROTOP}/etc/dblist.cfg | awk -F"|" '{print $2}' 2>/dev/null`
+      DB=`grep "^${IP1}|" $DBLIST | awk -F"|" '{print $2}' 2>/dev/null`
    fi
 
    # If DB has a value then we found a friendly name and the path is held in DB
@@ -142,4 +197,27 @@ else
       errmsg "${DB} does not exist."
    fi
 
+fi
+
+if [ $debug == "true" ]; then
+   echo "FILT = $FILT"
+   echo "LOGHIST = $LOGHIST"
+   echo "FLOGHIST = $FLOGHIST"
+   read x
+fi
+
+if $FILT = true
+then
+   if ! [[ "$LOGHIST" -eq "0" ]]
+   then
+      LHIST=$((LOGHIST*7))
+      echo "Cleaning up any archived log files older than $LOGHIST weeks"
+      $ECHO find ${LGARCDIR} -name "*.lg.[0-9]*" -mtime +$LHIST -exec rm {} \;
+   fi
+   if ! [[ "$FLOGHIST" -eq "0" ]]
+   then
+      LHIST=$((FLOGHIST*7))
+      echo "Cleaning up any filtered archived log files older than $FLOGHIST weeks"
+      $ECHO find ${LGARCDIR} -name "*.lg.[0-9]*" -mtime +$LHIST -exec rm {} \;
+   fi
 fi
